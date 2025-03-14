6bf9c93583882716cbb9c180661360b0b3fe2b39
NewOld21 <kshandy0221@gmail.com>
2025-03-14 20:13:42 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,11 +1,4 @@
-max = 0
-max_num = 0
-for i in range(9) :
-    x = int(input())
-    if max < x :
-        max = x
-        max_num = i
-print(max)
-print(max_num+1)
-
-        
+i=0
+data = [int(input()) for i in range(9)]
+print(max(data))
+print(data.index(max(data))+1)
