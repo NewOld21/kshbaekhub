c168def4fabf2c2b3129893c08763a52aa1e7abd
NewOld21 <kshandy0221@gmail.com>
2025-03-15 21:28:20 +0900

    [Bronze II] Title: 수 정렬하기, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,19 +1,7 @@
-a = [0] * 2001
+n = []
 for i in range(int(input())) :
-    n = int(input())
-    if n<0 :
-        a[1000+n] +=1
-    elif n==0 :
-        a[1000] += 1
-    else :
-        a[n+1000] +=1
-
-for i in range(len(a)) :
-    if i<1001 :
-        if a[i]>0 :
-            print(i-1000)
-    elif i==1000 :
-        print(0)
-    else :
-        if a[i]>0 :
-            print(i-1000)
+    a = int(input())
+    n.append(a)
+n.sort()
+for i in n :
+    print(i)
