5bdbfb5b86dfac6d3bc9491a34b09fc27d427f95
NewOld21 <kshandy0221@gmail.com>
2025-03-16 22:49:48 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8344 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,9 @@
+import sys
+n = int(sys.stdin.readline())
+a = [0] *10001
+for _ in range(n) :
+    a[int(sys.stdin.readline())] +=1 
+for i in range(len(a)) :
+    if a[i]!=0 :
+        for j in range(a[i]) :
+            print(i)
