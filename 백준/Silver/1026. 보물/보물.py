b48f2a9e7c5351f8406f9ce548060b5d3edea0bb
NewOld21 <kshandy0221@gmail.com>
2025-03-19 14:37:50 +0900

    [Silver IV] Title: 보물, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -6,5 +6,11 @@ A.sort()
 B.sort(reverse=True)
 cnt = 0 
 for i in range(N) :
-    cnt += A[i]*B[i]
+    max = 0
+    num = 0
+    for j in range(len(B)) :
+        if max<B[j] :
+            max = B[j]
+    cnt += A[i]*max
+    B.remove(max)
 print(cnt)
