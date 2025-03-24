080ee8756a69e6eb47e6f69f441ebe941c903ba4
NewOld21 <kshandy0221@gmail.com>
2025-03-24 14:42:11 +0900

    [Silver II] Title: 나무 자르기, Time: 436 ms, Memory: 258628 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,21 +1,19 @@
 import sys
-
-# 입력 받기
 N, M = map(int, sys.stdin.readline().split())
 tree = list(map(int, sys.stdin.readline().split()))
 
-# 이진 탐색을 위한 변수 설정
-low, high = 0, max(tree)
+pl = 0 
+pr = max(tree)
 
-# 이진 탐색 수행
-while low <= high:
-    mid = (low + high) // 2  # 절단기 높이 설정
-    cut = sum(t - mid for t in tree if t > mid)  # 자른 나무 길이 계산
+while pl<= pr :
+    pc = (pl + pr) // 2
+    cut = 0
+    for i in range(N) :
+        if tree[i] - pc > 0 :
+            cut += tree[i] - pc
+    if M<=cut :
+        pl = pc + 1
+    else :
+        pr = pc - 1
 
-    if cut >= M:
-        low = mid + 1  # 더 높은 높이도 가능할지 탐색
-    else:
-        high = mid - 1  # 너무 많이 잘랐으면 높이를 낮춤
-
-# 최적 절단기 높이 출력
-print(high)
+print(pr)
