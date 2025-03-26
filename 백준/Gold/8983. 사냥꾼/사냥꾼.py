06e025bd32d9174bd4c42a8671e379cf7235cb86
NewOld21 <kshandy0221@gmail.com>
2025-03-26 15:30:25 +0900

    [Gold IV] Title: 사냥꾼, Time: 568 ms, Memory: 51768 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -2,17 +2,31 @@ import sys
 M, N, L = map(int, sys.stdin.readline().split())
 Mx = list(map(int, sys.stdin.readline().split()))
 Nxy = []
-ans = 0
+ans = [0]
 for i in range(N) :
     x,y = map(int, sys.stdin.readline().split())
     Nxy.append([x,y])
+Mx.sort()
+def binary_s(x,y) :
+    pl = 0
+    pr = M-1
+    while pl<= pr:
+        pc = (pl+pr) //2
+        if (abs(Mx[pc]- x) +y) <= L :
+            ans[0] += 1
+            break
+        else : 
+            if Mx[pc]- x > 0 :
+                pr = pc - 1
+            else :
+                pl = pc + 1       
+    return
 
-for i in range(M) :
-    x = Mx[i]
-    for j in range(N) :
-        nx, ny = Nxy[j][0], Nxy[j][1]
-        if Nxy[j][0]>0 and Nxy[j][1]>0 and (abs(x-nx) + ny) <= L :
-            ans+= 1
-            Nxy[j][0], Nxy[j][1] = -1, -1
 
-print(ans)
+for i in range(N) :
+    x = Nxy[i][0]
+    y = Nxy[i][1]
+    binary_s(x,y)
+    
+
+print(ans[0])
