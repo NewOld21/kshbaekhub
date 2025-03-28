e71e7b5b75fa08de252223a06a0829e6f22ba72a
NewOld21 <kshandy0221@gmail.com>
2025-03-29 00:03:11 +0900

    [Gold III] Title: 아침 산책, Time: 792 ms, Memory: 104200 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,26 +1,35 @@
 import sys
+sys.setrecursionlimit(10**9)
 N = int(sys.stdin.readline())
 place = input()
 path = [[] for _ in range(N+1)]
+check = [0] * (N+1)
 for i in range(1,N) :
     a, b = map(int, sys.stdin.readline().split())
     path[a].append(b)
     path[b].append(a)
 
 def dfs(n) :
-    global check, cnt, place
-    check[n] = 1
+    res = 0
     for i in path[n] :
+        if place[i-1] == '0' :
+            if check[i] == 0 :
+                check[i] = 1
+                res += dfs(i)
+        else :
+            res += 1
+    return res
+            
+ans = 0
+for i in range(1, N+1) :
+    if place[i-1] == '0' :
         if check[i] == 0 :
             check[i] = 1
-            if place[i-1] == '1' :
-                cnt += 1
-                continue
+            cnt = dfs(i)
+            ans += cnt * (cnt-1)
             dfs(i)
-
-cnt = 0
-for i in range(1, N+1) :
-    if place[i-1] == '1' :
-        check = [0] * (N+1)
-        dfs(i)
-print(cnt)
+    else :
+        for j in path[i] :
+            if place[j-1] == '1' :
+                ans +=1
+print(ans)
