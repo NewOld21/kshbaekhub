025a26cbdeca208b9da50c52499881f206eec6f3
NewOld21 <kshandy0221@gmail.com>
2025-03-28 13:33:20 +0900

    [Silver I] Title: 트리 순회, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,82 +1,47 @@
-import sys 
+import sys
 N = int(sys.stdin.readline())
-p_tree = []
-c_tree = []
-
+tree = {}
 for i in range(N) :
     n, left ,right = sys.stdin.readline().split()
-    p_tree.append(n)
-    c_tree.append([left, right])
+    tree[n] = [left, right]
+
 Pre = []
+def preOder(n) :
+    if n=='.' :
+        return
+    
+    left, right = tree[n]
+    
+    Pre.append(n)
+    preOder(left)
+    preOder(right)
+
 In = []
+def inOder(n) :
+    if n=='.' :
+        return
+    
+    left, right = tree[n]
+    
+    inOder(left)
+    In.append(n)
+    inOder(right)
+
 Post = []
-#전위 순회회
-def preOder(num) :
-    root = p_tree[num]
-    left = c_tree[num][0]
-    right = c_tree[num][1]
-    Pre.append(root)
-    if left != '.' :
-        for i in range(N) :
-            if left == p_tree[i] :
-                left_idx = i
-                break
-        preOder(left_idx)
-    if right != '.' :
-        for i in range(N) :
-            if right == p_tree[i] :
-                right_idx = i
-                break
-        preOder(right_idx)
+def postOder(n) :
+    if n=='.' :
+        return
     
-    return
+    left, right = tree[n]
+    
+    postOder(left)
+    postOder(right)
+    Post.append(n)
 
-preOder(0)
+preOder('A')
+inOder('A')
+postOder('A')
+
 print(''.join(Pre))
-
-#중위 순회
-def inOder(num) :
-    root = p_tree[num]
-    left = c_tree[num][0]
-    right = c_tree[num][1]
-    if left != '.' :
-        for i in range(N) :
-            if left == p_tree[i] :
-                left_idx = i
-                break
-        inOder(left_idx)
-    In.append(root)
-    if right != '.' :
-        for i in range(N) :
-            if right == p_tree[i] :
-                right_idx = i
-                break
-        inOder(right_idx)
-    
-    return
-
-inOder(0)
 print(''.join(In))
-
-#후위위 순회
-def postOder(num) :
-    root = p_tree[num]
-    left = c_tree[num][0]
-    right = c_tree[num][1]
-    if left != '.' :
-        for i in range(N) :
-            if left == p_tree[i] :
-                left_idx = i
-                break
-        postOder(left_idx)
-    if right != '.' :
-        for i in range(N) :
-            if right == p_tree[i] :
-                right_idx = i
-                break
-        postOder(right_idx)
-    Post.append(root)
-    return
-
-postOder(0)
 print(''.join(Post))
