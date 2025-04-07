8b5b2c0957d0ecc95469a403c3d41e0c6b3516dc
NewOld21 <kshandy0221@gmail.com>
2025-04-07 23:36:21 +0900

    [Bronze II] Title: 우유 축제, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14720. 우유 축제/우유 축제.py
+++ 백준/Bronze/14720. 우유 축제/우유 축제.py
@@ -1,10 +1,13 @@
 import sys
 N = int(sys.stdin.readline())
-milk = list(map(int, sys.stdin.readline().split()))
+m = list(map(int,sys.stdin.readline().split() ))
+
 type = [0,1,2]
-cnt = 0 
-for i in milk :
-    if i == type[cnt%3] :
+cnt = 0
+t=0
+for i in range(N) :
+    if m[i] == type[t%3] :
         cnt += 1
+        t += 1
 
 print(cnt)
