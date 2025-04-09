b88f060bd0108bc4000445ad95d72538580440d6
NewOld21 <kshandy0221@gmail.com>
2025-04-09 21:13:24 +0900

    [Gold IV] Title: 점프, Time: 800 ms, Memory: 70916 KB -BaekjoonHub

--- 백준/Gold/2253. 점프/점프.py
+++ 백준/Gold/2253. 점프/점프.py
@@ -1,24 +1,21 @@
 import sys
-from collections import deque
-
 N, M = map(int, sys.stdin.readline().split())
-check = [[] for _ in range(N + 1)]
-small_rock = set()
+dp = [[sys.maxsize]*(150) for _ in range(N + 1)] 
+dp[1][0] = 0
+
+stone_set = set()
 for _ in range(M):
-    small = int(sys.stdin.readline())
-    small_rock.add(small)
-ans = -1
-queue = deque([(1, 0, 0)])
-while queue:
-    location, jump, n = queue.popleft()
-    for x in [jump + 1, jump, jump - 1]:
-        if x > 0:
-            next_location = location + x
-            if next_location == N:
-                    ans = n + 1
-                    queue.clear()
-                    break
-            if next_location < N and next_location not in small_rock and x not in check[next_location]:
-                check[next_location].append(x)
-                queue.append((next_location, x, n + 1))
-print(ans)
+    stone_set.add(int(sys.stdin.readline()))
+
+
+for i in range(2, N + 1):
+    if i in stone_set:
+        continue
+    for j in range(1, 145):   # i = 위치 , j = 점프 거리
+        if i>j :
+            dp[i][j] = min(dp[i - j][j - 1], dp[i - j][j], dp[i - j][j + 1]) + 1
+
+if min(dp[N]) == sys.maxsize:
+    print(-1)
+else:
+    print(min(dp[N]))
