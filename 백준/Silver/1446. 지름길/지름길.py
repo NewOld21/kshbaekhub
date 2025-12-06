df5b53fd064e991006b302bc4ec4975b817d8a01
NewOld21 <kshandy0221@gmail.com>
2025-12-06 14:43:40 +0900

    [Silver I] Title: 지름길, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,23 +1,22 @@
 import sys
-import heapq
+
 N, D = map(int, sys.stdin.readline().split())
 
-road = [[] for _ in range(D+1)]
-distance = [sys.maxsize for _ in range(D+1)]
-distance[0] = 0
-for _ in range(N) :
-    x,y,z = map(int, sys.stdin.readline().split())
-    if y <= D : 
-        road[x].append([y,z])
+road = []
 
-for i in range(0,D+1) :
+for i in range(N) :
+    s, e, d = map(int, sys.stdin.readline().split())
+    if e <= D :
+        road.append([s,e,d])
+
+dp = [sys.maxsize for _ in range(D+1)]
+dp[0] = 0
+
+for i in range(D+1) :
     if i > 0 :
-        distance[i] = min(distance[i], distance[i-1] + 1)
-    for e,w in road[i] :
-        distance[e] = min(distance[i] + w, distance[e])
-
-
-print(distance[-1])
-
-
-
+        dp[i] = min(dp[i], dp[i-1] + 1)
+    for s, e, d in road :
+        if i == s :
+            dp[e] = min(dp[e], dp[i] + d)
+    
+print(dp[-1])
