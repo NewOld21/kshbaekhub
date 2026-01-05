b494e66919ab5d973bffa0a7738074a83b3c8772
NewOld21 <kshandy0221@gmail.com>
2026-01-05 10:28:52 +0900

    [Silver II] Title: 우승자는 누구?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/5179. 우승자는 누구？/우승자는 누구？.py
+++ 백준/Silver/5179. 우승자는 누구？/우승자는 누구？.py
@@ -5,44 +5,38 @@ K = int(input())
 for test in range(K):
     M, N, P = map(int, input().split())
     
-    # 각 참가자별로 [각 문제 점수, 각 문제 정답여부, 푼 문제 수]
-    participants = {}
+    scores = [[0] * M for _ in range(P+1)]      
+    solved = [[False] * M for _ in range(P+1)]  
+    solve_count = [0] * (P+1)                   
     
     for _ in range(N):
         p, m, t, j = input().split()
         p = int(p)
         t = int(t)
         j = int(j)
-        m_index = ord(m) - 65  # A=0, B=1, ...
+        m_index = ord(m) - 65 
         
-        if p not in participants:
-            participants[p] = [[0] * M, [False] * M, 0]
-        
-        if j == 1 and not participants[p][1][m_index]:  # 정답이고 아직 안 맞춘 문제
-            participants[p][1][m_index] = True
-            participants[p][2] += 1
-            participants[p][0][m_index] += t  # 시간만 더함
-        elif j == 0 and not participants[p][1][m_index]:  # 오답이고 아직 안 맞춘 문제
-            participants[p][0][m_index] += 20  # 패널티만 더함
+        if j == 1 and not solved[p][m_index]:  
+            solved[p][m_index] = True
+            solve_count[p] += 1
+            scores[p][m_index] += t 
+        elif j == 0 and not solved[p][m_index]: 
+            scores[p][m_index] += 20 
     
-    # 결과 계산
     result = []
-    for participant_id in range(1, P+1):
-        if participant_id in participants:
-            data = participants[participant_id]
-            total_time = sum(data[0][i] for i in range(M) if data[1][i])  # 맞춘 문제만 합산
-            solved_count = data[2]
-        else:
-            total_time = 0
-            solved_count = 0
-        result.append((participant_id, solved_count, total_time))
+    for p in range(1, P+1):
+        total_time = 0
+        for m in range(M):
+            if solved[p][m]: 
+                total_time += scores[p][m]
+        result.append((p, solve_count[p], total_time))
     
-    # 정렬: 푼 문제 수 내림차순, 총점 오름차순
+
     result.sort(key=lambda x: (-x[1], x[2]))
     
     print(f'Data Set {test + 1}:')
-    for participant_id, solved, total in result:
-        print(participant_id, solved, total)
+    for participant_id, solved_cnt, total in result:
+        print(participant_id, solved_cnt, total)
     
-    if test != K - 1:  # 마지막 테스트케이스가 아닐 때만
+    if test != K - 1:
         print()
