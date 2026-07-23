112a1e162b0fa94ee37301865ced499e665e42f7
NewOld21 <kshandy0221@gmail.com>
2026-07-23 13:11:39 +0900

    [level 2] Title: 올바른 괄호, Time: 2.73 ms, Memory: 57.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,18 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = false;
+        int cnt = 0;
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(cnt < 0)
+                break;
+            if(c=='(')
+                cnt++;
+            else
+                cnt--;
+        }
+        if(cnt==0)
+            answer = true;
+        return answer;
+    }
+}
